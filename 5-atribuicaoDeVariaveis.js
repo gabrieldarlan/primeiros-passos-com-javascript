27c758c8c9c69a9ba16eab4a1512c6c38681b151
gabrieldarlan <gabriel.darlan88@gmail.com>
2021-03-22 22:47:45 -0300

    capitulo 03 completo

--- 5-atribuicaoDeVariaveis.js
+++ 5-atribuicaoDeVariaveis.js
@@ -2,10 +2,13 @@ console.log("Trabalhando com atribuição de variáveis");
 //quando a variavel é declarada com "let" ela pode sofrer alteração durante o processamento
 //quando a variável é declarada com "const" ela não poderá mudar durante o processamento ela é imutavel
 
-const idade = 29;
+// const idade = 29;
 const primeiroNome = "Ricardo";
 const sobrenome = "Bugan";
 
+let contador = 0;
+contador = contador + 1;
+console.log(contador);
 
 // console.log(nome + " " + sobrenome);
 // console.log(nome, sobrenome);
@@ -15,3 +18,8 @@ console.log(`Meu nome é ${primeiroNome} ${sobrenome}`);
 //sobrescrita
 const nomeCompleto = primeiroNome + sobrenome;
 console.log(nomeCompleto);
+
+let idade; //declarando variável
+idade = 26; //atribuindo valor
+idade = idade + 1;
+console.log(idade);
