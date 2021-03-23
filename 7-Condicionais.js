a55aa0373e9cf6a859d31c7785294402a899b2dd
gabrieldarlan <gabriel.darlan88@gmail.com>
2021-03-23 17:26:27 -0300

    capitulo 04 completo

--- 7-Condicionais.js
+++ 7-Condicionais.js
@@ -5,20 +5,43 @@ const listaDeDestinos = new Array(
 );
 
 const idadeComprador = 18;
+const menorDeIdadeAcompanhado = true;
+const maiorDeIdade = idadeComprador >= 18;
+const temPassagemComprada = true;
+
 console.log(`Destinos possíveis:`);
 console.log(listaDeDestinos);
 
 
 
-if (idadeComprador >= 18) {
-    console.log(`Comprador maior de idade`);
-    console.log(listaDeDestinos);
+// if (idadeComprador >= 18) {
+//     console.log(`Comprador maior de idade`);
+//     console.log(listaDeDestinos);
+// } else if (estaAcompanhada) {
+//     console.log(`Comprador está acompanhado`);
+//     console.log(listaDeDestinos.splice(1, 1));
+// } else {
+//     console.log(`Comprador menor de idade e não posso vender`);
+
+// }
+
+if ((maiorDeIdade) || (menorDeIdadeAcompanhado)) {
+    console.log(`Pode comprar, boa viagem`);
+    console.log(listaDeDestinos.splice(2, 1));
 } else {
-    console.log(`Comprador menor de idade e não posso vender`);
+    console.log(`Comprador não pode comprar`);
+
+}
+console.log("Embarque:\n\n");
+if (idadeComprador >= 18 && temPassagemComprada) {
+    console.log("Boa viagem");
+} else {
+    console.log("Não pode embarcar");
 }
 
-console.log(idadeComprador > 18);
-console.log(idadeComprador < 18);
-console.log(idadeComprador <= 18);
-console.log(idadeComprador >= 18);
-console.log(idadeComprador == 18);
+
+// console.log(idadeComprador > 18);
+// console.log(idadeComprador < 18);
+// console.log(idadeComprador <= 18);
+// console.log(idadeComprador >= 18);
+// console.log(idadeComprador == 18);
